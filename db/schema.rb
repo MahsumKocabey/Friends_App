b74d163245b462a95eaeccb6beb908a17614941b
Mahsum Kocabey <mahsumkocabey@hotmail.com>
2020-11-19 23:48:36 +0000

    initial commit

---
+++ db/schema.rb
@@ -0,0 +1,23 @@
+ActiveRecord::Schema.define(version: 2020_11_19_221150) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
